75ec9d53a3094c8ef5a80dc2634088c1c0b46e8a
sarahcomella <sarahcomella5@gmail.com>
2022-03-19 13:03:45 -0500

    Fixes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("does this work");
+
+$('#support').mouseover(function(){
+    $(this).css("font-weight","900");
+  });
+
+  $('#support').mouseout(function(){
+    $(this).css("font-weight","500");
+  });
